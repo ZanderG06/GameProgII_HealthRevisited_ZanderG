3d7ed956442c2b15140093e925174238dff9a11a
Zander Goodwin <zanderngoodwin@gmail.com>
2026-01-13 22:25:30 -0400

    Fixed Set to be public

--- GameProgII_HealthRevisited_ZanderG/Program.cs
+++ GameProgII_HealthRevisited_ZanderG/Program.cs
@@ -76,7 +76,7 @@ namespace GameProgII_HealthRevisited_ZanderG
         public string Name
         {
             get { return _name; }
-            private set { _name = value; }
+            set { _name = value; }
         }
 
         public Health Health
